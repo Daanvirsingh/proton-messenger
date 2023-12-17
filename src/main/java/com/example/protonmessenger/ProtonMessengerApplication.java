452b6703ac7c352ddacaeed6387520ce59410db5
Daanvir91Social <104888143+Daanvir91Social@users.noreply.github.com>
2023-12-17 20:58:33 +0530

    spring boot gradle poject

---
+++ src/main/java/com/example/protonmessenger/ProtonMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.protonmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtonMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtonMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/protonmessenger/ProtonMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protonmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtonMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
