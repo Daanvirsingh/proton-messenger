25fd31887cad8c89546ee52f88883222c2a5dcad
Daanvir91Social <104888143+Daanvir91Social@users.noreply.github.com>
2023-12-18 00:56:28 +0530

    added user repository and Gradle build process in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
+# Stage 1: Build the application
+FROM gradle:7.4.2-jdk17 as build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build --no-daemon
+
+# Stage 2: Create the Docker final image
 FROM openjdk:17-oracle
 EXPOSE 8080
-ADD build/libs/proton-messenger-0.0.1-SNAPSHOT.jar app.jar
+COPY --from=build /home/gradle/src/build/libs/proton-messenger-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/example/protonmessenger/contoller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.protonmessenger.contoller;
+
+import com.example.protonmessenger.model.User;
+import com.example.protonmessenger.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class UserController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user")
+    Iterable<User> all() {
+        return userRepository.findAll();
+    }
+
+}
